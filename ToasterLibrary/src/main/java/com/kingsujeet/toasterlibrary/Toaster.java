70ba3a226726eb85a8a2b46680a96345c07e0ce3
Sujeet Thakur <mydeveloper.sujeet@gmail.com>
2021-05-24 18:49:49 +0530

    completed commit

---
+++ ToasterLibrary/src/main/java/com/kingsujeet/toasterlibrary/Toaster.java
@@ -0,0 +1,14 @@
+package com.kingsujeet.toasterlibrary;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class Toaster {
+
+    public static void s(Context c, String msg){
+
+        Toast.makeText(c,msg,Toast.LENGTH_SHORT).show();
+
+    }
+
+}
